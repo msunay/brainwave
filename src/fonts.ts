eb0402382d712e3ebdaade335b2d37207374f3c1
Alex <78169857+msunay@users.noreply.github.com>
2024-04-05 19:37:39 +0100

    feat: scaffold + header

---
+++ src/fonts.ts
@@ -0,0 +1,6 @@
+import { Rubik, Montserrat } from "next/font/google";
+
+const rubik = Rubik({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export { rubik, montserrat }
