61356b209d380fb9e9983e5e716646f6812932a8
Alex <78169857+msunay@users.noreply.github.com>
2024-04-05 20:12:45 +0100

    feat: icons for grid items

--- src/components/featuresSection/featuresSection.tsx
+++ src/components/featuresSection/featuresSection.tsx
@@ -2,7 +2,13 @@
 
 import React from "react";
 import styles from "./featuresSection.module.css";
-
+import Image from "next/image";
+import preferencesCircle from "@public/icons/preferences-circle.png";
+import barChart from "@public/icons/chart-bar-33.png";
+import coupon from "@public/icons/coupon.png";
+import user from "@public/icons/a-sync.png";
+import layers from "@public/icons/ic_layers_48px.png";
+import shoppingCart from "@public/icons/ic_shopping_cart_48px.png";
 
 export default function FeaturesSection() {
   const gridItemText =
@@ -21,36 +27,54 @@ export default function FeaturesSection() {
       </div>
       <div className={styles.featuresGridContainer}>
         <div className={styles.gridItemContainer}>
+          <div className={styles.imageContainer}>
+            <Image src={preferencesCircle} alt="Preferences Icon" />
+          </div>
           <div className={styles.textContainer}>
             <h2 className={styles.gridItemTitle}>Quick setup process</h2>
             <p className={styles.gridItemText}>{gridItemText}</p>
           </div>
         </div>
         <div className={styles.gridItemContainer}>
+          <div className={styles.imageContainer}>
+            <Image src={shoppingCart} alt="Shopping Cart Icon" />
+          </div>
           <div className={styles.textContainer}>
             <h2 className={styles.gridItemTitle}>Create custom orders</h2>
             <p className={styles.gridItemText}>{gridItemText}</p>
           </div>
         </div>
         <div className={styles.gridItemContainer}>
+          <div className={styles.imageContainer}>
+            <Image src={coupon} alt="Coupon Icon" />
+          </div>
           <div className={styles.textContainer}>
             <h2 className={styles.gridItemTitle}>Multiple coupons</h2>
             <p className={styles.gridItemText}>{gridItemText}</p>
           </div>
         </div>
         <div className={styles.gridItemContainer}>
+          <div className={styles.imageContainer}>
+            <Image src={layers} alt="Layers Icon" />
+          </div>
           <div className={styles.textContainer}>
             <h2 className={styles.gridItemTitle}>Organize your campaigns</h2>
             <p className={styles.gridItemText}>{gridItemText}</p>
           </div>
         </div>
         <div className={styles.gridItemContainer}>
+          <div className={styles.imageContainer}>
+            <Image src={user} alt="User Icon" />
+          </div>
           <div className={styles.textContainer}>
             <h2 className={styles.gridItemTitle}>Manage Customers</h2>
             <p className={styles.gridItemText}>{gridItemText}</p>
           </div>
         </div>
         <div className={styles.gridItemContainer}>
+          <div className={styles.imageContainer}>
+            <Image src={barChart} alt="Chart Icon" />
+          </div>
           <div className={styles.textContainer}>
             <h2 className={styles.gridItemTitle}>Track progress fast</h2>
             <p className={styles.gridItemText}>{gridItemText}</p>
